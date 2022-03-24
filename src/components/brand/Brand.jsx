de4112ee925453d2a7b5590691c8427678defcb2
addydist <90037838+addydist@users.noreply.github.com>
2022-03-24 12:30:49 +0530

    Update Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { google, slack, atlassian, dropbox, shopify } from './imports';
 import './brand.css';
 
-const Brand = () => (
+const Brand = () => {
+  return(
   <div className="gpt3__brand section__padding">
     <div>
       <img src={google} />
@@ -21,5 +22,5 @@ const Brand = () => (
     </div>
   </div>
 );
-
+}
 export default Brand;
